d74a51a7fb362c3700f3a0ba9703ffcdb7ead949
Alex Bates <alexbates828@gmail.com>
2020-06-06 17:49:17 -0500

    Initial commit from Spring Initializr

---
+++ src/test/kotlin/ly/eagle/messaging/MessagingApplicationTests.kt
@@ -0,0 +1,13 @@
+package ly.eagle.messaging
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MessagingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
